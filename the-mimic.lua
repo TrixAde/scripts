df3f90c11f0a810f29d001c9dc4a0336775d3014
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 22:03:00 +0200

    Create the-mimic.lua

---
+++ the-mimic.lua
@@ -0,0 +1,5 @@
+pcall(loadstring(game:HttpGet("https://pastebin.com/raw/SyjxHNYs")))
+--first execute that for start a game
+
+pcall(loadstring(game:HttpGet("https://pastebin.com/raw/yyiEnPBX")))
+--secondly execute that for auto finish
