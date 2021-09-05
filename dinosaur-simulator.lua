2738c127c840f3ed542ce1e5910c3d963fcfa882
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 21:41:23 +0200

    Create dinosaur-simulator.lua

---
+++ dinosaur-simulator.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/MaGiXx228/scripts/main/Dinosaur%20Simulator%20v2.0.lua')))()
+------------------------------------------------------------------------------------------------------------------------------------
+loadstring(game:GetObjects("rbxassetid://3297501610")[1].Source)()
