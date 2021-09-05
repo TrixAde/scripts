6fe6bb4548f0fc3fb4347ee6b2d45da5c8195c56
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-05 21:50:52 +0200

    Create muscle-legends.lua

---
+++ muscle-legends.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/littlegui/main/MuscleLegends"))()
+------------------------------------------------------------------------------------------------
+loadstring(game:HttpGet("https://pastebin.com/raw/7wWhp6EP"))()
