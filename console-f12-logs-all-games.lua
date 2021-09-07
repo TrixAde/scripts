e1ab105ab7cfa163d30e0acd00024f9947aa637c
Trix <80095537+TrixAde@users.noreply.github.com>
2021-09-07 17:39:47 +0200

    Update console-f12-logs-all-games.lua

--- console-f12-logs-all-games.lua
+++ console-f12-logs-all-games.lua
@@ -1,11 +1,9 @@
 pcall(loadstring(game:HttpGet("https://cdn.julman.fr/scripts/13.lua")))
 
 --console logs script
-
 ---------------------------------------------------------------------------------------------------------------
 loadstring(game:GetObjects('rbxassetid://2180084478')[1].Source)()
 
------------------------------------------------------------------------------------------------------------------
 pcall(loadstring(game:HttpGet("https://cdn.julman.fr/scripts/36.lua")))
 
 --f12 scripts
